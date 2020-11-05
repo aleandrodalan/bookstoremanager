84fca0a7502720300ed532ce9e5996e96f2a0d54
Aleandro Pereira Dalan <aleandrodalan@gmail.com>
2020-11-04 21:38:16 -0400

    Created book repository class

---
+++ src/main/java/com/aleandrodalan/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.aleandrodalan.bookstoremanager.repository;
+
+import com.aleandrodalan.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
