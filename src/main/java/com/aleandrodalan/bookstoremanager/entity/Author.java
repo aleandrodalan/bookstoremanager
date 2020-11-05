35fe7a145cab5271afbf6b82c96525468e58d9c5
Aleandro Pereira Dalan <aleandrodalan@gmail.com>
2020-11-04 21:32:17 -0400

    Developed project entities

---
+++ src/main/java/com/aleandrodalan/bookstoremanager/entity/Author.java
@@ -0,0 +1,31 @@
+package com.aleandrodalan.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
